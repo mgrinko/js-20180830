4960a9d120907962e02dc097eeabba3b58b222ce
Nikolai <nlv.nki@gmail.com>
2018-10-18 12:15:56 +0300

    Added phoneDetails processing

--- scripts/phones/components/phone-viewer.js
+++ scripts/phones/components/phone-viewer.js
@@ -9,6 +9,7 @@ export default class PhoneViewer extends Component{
 
   show(phoneDetails) {
     this._phone = phoneDetails;
+   // console.log(this._phone)
     this._render();
   }
 
@@ -28,38 +29,65 @@ export default class PhoneViewer extends Component{
         }
     }
 
-  _render() {
-    this._element.innerHTML = `
-      <img class="phone large-img" src="img/phones/motorola-xoom-with-wi-fi.0.jpg">
+    _render() {
 
-      <button>Back</button>
-      <button>Add to basket</button>
-  
-  
-      <h1>Motorola XOOM™ with Wi-Fi</h1>
-  
-      <p>Motorola XOOM with Wi-Fi has a super-powerful dual-core processor and Android™ 3.0 (Honeycomb) — the Android platform designed specifically for tablets. With its 10.1-inch HD widescreen display, you’ll enjoy HD video in a thin, light, powerful and upgradeable tablet.</p>
-  
-      <ul class="phone-thumbs">
-        <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.0.jpg">
-        </li>
-        <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.1.jpg">
-        </li>
-        <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.2.jpg">
-        </li>
-        <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.3.jpg">
-        </li>
-        <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.4.jpg">
-        </li>
-        <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.5.jpg">
-        </li>
-      </ul>
-    `;
-  }
+        console.log( this._phone )
+        this._element.innerHTML = `
+           <img class="phone large-img" src="${this._phone.images[0]}">      
+           <button>Back</button>      
+           <button>Add to basket</button>
+           
+            <h1>${this._phone.name}</h1>
+            <p>${this._phone.description}</p>
+            <ul class="phone-thumbs">
+            ${this._phone.images.map(image => `
+            
+            <li>
+              <img src=${image}>
+            
+            </li>
+            
+            
+            `)}
+            </ul>
+       
+   
+         `;
+    }
+
+  //
+  // _render() {
+  //   this._element.innerHTML = `
+  //     <img class="phone large-img" src="img/phones/motorola-xoom-with-wi-fi.0.jpg">
+  //
+  //     <button>Back</button>
+  //     <button>Add to basket</button>
+  //
+  //
+  //     <h1>Motorola XOOM™ with Wi-Fi</h1>
+  //
+  //     <p>Motorola XOOM with Wi-Fi has a super-powerful dual-core processor and Android™ 3.0 (Honeycomb) — the Android platform designed specifically for tablets. With its 10.1-inch HD widescreen display, you’ll enjoy HD video in a thin, light, powerful and upgradeable tablet.</p>
+  //
+  //     <ul class="phone-thumbs">
+  //       <li>
+  //         <img src="img/phones/motorola-xoom-with-wi-fi.0.jpg">
+  //       </li>
+  //       <li>
+  //         <img src="img/phones/motorola-xoom-with-wi-fi.1.jpg">
+  //       </li>
+  //       <li>
+  //         <img src="img/phones/motorola-xoom-with-wi-fi.2.jpg">
+  //       </li>
+  //       <li>
+  //         <img src="img/phones/motorola-xoom-with-wi-fi.3.jpg">
+  //       </li>
+  //       <li>
+  //         <img src="img/phones/motorola-xoom-with-wi-fi.4.jpg">
+  //       </li>
+  //       <li>
+  //         <img src="img/phones/motorola-xoom-with-wi-fi.5.jpg">
+  //       </li>
+  //     </ul>
+  //   `;
+  // }
 }
