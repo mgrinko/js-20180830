17df818980ef8ab773c91dd331956e54a14b4971
mgrinko <glukym@gmail.com>
2018-10-25 19:17:21 +0300

    Basic promise object was added

--- scripts/http-service.js
+++ scripts/http-service.js
@@ -2,12 +2,9 @@ const API_URL = 'https://mgrinko.github.io/js-20180830';
 // const API_URL = 'http://localhost:3000'
 
 const HttpService = {
-  sendRequest(url, {
-    method = 'GET',
-    successCallback = () => {},
-    errorCallback = (error) => { console.error(error) },
-  }) {
+  sendRequest(url, callback) {
     let xhr = new XMLHttpRequest();
+    let method = 'GET';
 
     xhr.open(method, `${API_URL}/api/${url}`, true);
 
@@ -15,11 +12,11 @@ const HttpService = {
 
     xhr.onload = () => {
       if (xhr.status !== 200) {
-        errorCallback( xhr.status + ': ' + xhr.statusText );
+        console.log( xhr.status + ': ' + xhr.statusText );
       } else {
         let data = JSON.parse(xhr.responseText);
 
-        successCallback(data);
+        callback(data);
       }
     };
   }
--- scripts/phones/services/phone-service.js
+++ scripts/phones/services/phone-service.js
@@ -2,16 +2,33 @@ import HttpService from '../../http-service.js'
 
 const PhoneService = {
   getPhones(callback) {
-    HttpService.sendRequest('phones.json', {
-      successCallback: callback,
-    });
+    HttpService.sendRequest('phones.json', callback);
   },
 
   getPhone(phoneId, callback) {
-    HttpService.sendRequest(`phones/${phoneId}.json`, {
-      successCallback: callback,
-    });
+    let promise = this._sendRequest(`phones/${phoneId}.json`);
+
+    promise.then(callback);
   },
+
+
+  _sendRequest(url) {
+    let promise = {
+      then(successCallback) {
+        this.successCallback = successCallback;
+      },
+
+      resolve(data) {
+        this.successCallback(data);
+      }
+    };
+
+    HttpService.sendRequest(url, (data) => {
+      promise.resolve(data);
+    });
+
+    return promise;
+  }
 };
 
 export default PhoneService;
