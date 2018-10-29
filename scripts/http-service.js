5c58c1a620edea08ba30fdb5939dea674918b4f0
mgrinko <glukym@gmail.com>
2018-10-29 17:57:39 +0200

    fetch was added

--- scripts/http-service.js
+++ scripts/http-service.js
@@ -1,71 +1,10 @@
 const API_URL = 'https://mgrinko.github.io/js-20180830';
 // const API_URL = 'http://localhost:3000'
 
-class MyPromise {
-  constructor (executor) {
-    this._successCallbacks = [];
-    this._errorCallbacks = [];
-    this._status = 'pending';
-    this._result = undefined;
-
-    executor(this._resolve.bind(this), this._reject.bind(this));
-  }
-
-  then(successCallback) {
-    if (this._status === 'pending') {
-      this._successCallbacks.push(successCallback);
-    } else {
-      successCallback(this._result);
-    }
-  }
-
-  catch(errorCallback) {
-    if (this._status === 'pending') {
-      this._errorCallbacks.push(errorCallback);
-    } else {
-      errorCallback(this._result);
-    }
-  }
-
-  _resolve(data) {
-    this._status = 'fulfilled';
-    this._result = data;
-
-    this._successCallbacks.forEach(callback => {
-      callback(data);
-    });
-  }
-
-  _reject(error) {
-    this._status = 'rejected';
-    this._result = error;
-
-    this._errorCallbacks.forEach(callback => {
-      callback(error);
-    });
-  }
-}
-
 const HttpService = {
   sendRequest(url) {
-    return new Promise((resolve, reject) => {
-      let xhr = new XMLHttpRequest();
-      let method = 'GET';
-
-      xhr.open(method, `${API_URL}/api/${url}`, true);
-
-      xhr.send();
-
-      xhr.onload = () => {
-        if (xhr.status !== 200) {
-          reject( xhr.status + ': ' + xhr.statusText );
-        } else {
-          let data = JSON.parse(xhr.responseText);
-
-          resolve(data);
-        }
-      };
-    });
+    return fetch(`${API_URL}/api/${url}`)
+      .then(response => response.json());
   }
 };
 
