1efe1c46301257b0c013b3bac5ac97c052d8f6a7
mgrinko <glukym@gmail.com>
2018-10-29 18:51:30 +0200

    Webpack was added

--- scripts/http-service.js
+++ scripts/http-service.js
@@ -2,9 +2,10 @@ const API_URL = 'https://mgrinko.github.io/js-20180830';
 // const API_URL = 'http://localhost:3000'
 
 const HttpService = {
-  sendRequest(url) {
-    return fetch(`${API_URL}/api/${url}`)
-      .then(response => response.json());
+  async sendRequest(url) {
+    let response = await fetch(`${API_URL}/api/${url}`)
+
+    return response.json();
   }
 };
 
---
+++ webpack.config.js
@@ -0,0 +1,96 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'none',
+  entry: './scripts/app.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'build.js'
+  },
+
+  devtool: 'source-map',
+  watch: true,
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          }
+        }
+      }
+    ]
+  }
+};
+
+
+
+
+
+// const webpack = require('webpack');
+// const path = require('path');
+// const isProd = process.env.NODE_ENV === 'production'
+// const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+//
+// let plugins = [
+//   new webpack.DefinePlugin({
+//     API_URL: isProd
+//       ? "'https://mgrinko.github.io/js-20180614/api/'"
+//       : "'http://localhost:3000/api/'",
+//   })
+// ];
+//
+// if (isProd) {
+//   plugins.push(
+//     new UglifyJsPlugin({
+//       sourceMap: isProd ? false : true,
+//     })
+//   )
+// }
+//
+// module.exports = {
+//   mode: 'none',
+//
+//   entry: './scripts/app.js',
+//   output: {
+//     path: path.resolve(__dirname, 'public'),
+//     filename: 'build.js'
+//   },
+//
+//   devtool: isProd ? false : 'source-map',
+//
+//   module: {
+//     rules: [
+//       {
+//         test: /\.hbs$/,
+//         loader: "handlebars-loader"
+//       },
+//       {
+//         test: /\.css$/,
+//         use: [ 'style-loader', 'css-loader' ]
+//       },
+//       {
+//         test: /\.js$/,
+//         exclude: /(node_modules)/,
+//         use: {
+//           loader: 'babel-loader',
+//           options: {
+//             presets: ['@babel/preset-env'],
+//             plugins: ['@babel/plugin-transform-runtime'],
+//           }
+//         }
+//       }
+//     ]
+//   },
+//
+//   plugins: plugins,
+//
+//   devServer: {
+//     contentBase: './public'
+//   }
+// };
