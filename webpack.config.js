fd75bfffebcf29c15b8c5782921e8ae54a51b272
mgrinko <glukym@gmail.com>
2018-10-29 19:35:16 +0200

    Minification was added

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-const path = require('path');
+const path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   mode: 'none',
@@ -35,75 +36,16 @@ module.exports = {
     ]
   },
 
+  plugins: [
+    new UglifyJsPlugin()
+  ],
+
+  optimization: {
+    minimizer: [new UglifyJsPlugin()]
+  },
+
   devServer: {
     contentBase: path.join(__dirname, 'public'),
     port: 9000
   }
 };
-
-
-
-
-
-// const webpack = require('webpack');
-// const path = require('path');
-// const isProd = process.env.NODE_ENV === 'production'
-// const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-//
-// let plugins = [
-//   new webpack.DefinePlugin({
-//     API_URL: isProd
-//       ? "'https://mgrinko.github.io/js-20180614/api/'"
-//       : "'http://localhost:3000/api/'",
-//   })
-// ];
-//
-// if (isProd) {
-//   plugins.push(
-//     new UglifyJsPlugin({
-//       sourceMap: isProd ? false : true,
-//     })
-//   )
-// }
-//
-// module.exports = {
-//   mode: 'none',
-//
-//   entry: './scripts/app.js',
-//   output: {
-//     path: path.resolve(__dirname, 'public'),
-//     filename: 'build.js'
-//   },
-//
-//   devtool: isProd ? false : 'source-map',
-//
-//   module: {
-//     rules: [
-//       {
-//         test: /\.hbs$/,
-//         loader: "handlebars-loader"
-//       },
-//       {
-//         test: /\.css$/,
-//         use: [ 'style-loader', 'css-loader' ]
-//       },
-//       {
-//         test: /\.js$/,
-//         exclude: /(node_modules)/,
-//         use: {
-//           loader: 'babel-loader',
-//           options: {
-//             presets: ['@babel/preset-env'],
-//             plugins: ['@babel/plugin-transform-runtime'],
-//           }
-//         }
-//       }
-//     ]
-//   },
-//
-//   plugins: plugins,
-//
-//   devServer: {
-//     contentBase: './public'
-//   }
-// };
